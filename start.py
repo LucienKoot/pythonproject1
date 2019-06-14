916e446e73d125e22e7e7d8d7b67b08825552bf4
Ludelaplu <lucien.koot@gmail.com>
2019-06-14 15:34:03 +0200

    test in list appending

--- start.py
+++ start.py
@@ -1,11 +1,9 @@
-list1 = ['nr1', 'nr2', 'nr3']
-list2 = [1000, 2000, 3000]
-print (list1 + list2)
 
 list1 =["item1", "item2", "item3", "item4", "item5"]
 list2 =[1000, 2000,]
 list1.remove("item5")
 list2.remove(2000)
+list2.append(3000)
 print(list1 + list2)
 
 
