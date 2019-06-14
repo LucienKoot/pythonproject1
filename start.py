7c3ca7aa67a4e6a2ec9e07d99df1fdf6942262c1
Ludelaplu <lucien.koot@gmail.com>
2019-06-14 15:15:45 +0200

    test in list remove

--- start.py
+++ start.py
@@ -1,3 +1,12 @@
 list1 = ['nr1', 'nr2', 'nr3']
 list2 = [1000, 2000, 3000]
-print (list1)
+print (list1 + list2)
+
+list1 =["item1", "item2", "item3", "item4", "item5"]
+list2 =[1000, 2000,]
+list1.remove("item5")
+list2.remove(2000)
+print(list1 + list2)
+
+
+
