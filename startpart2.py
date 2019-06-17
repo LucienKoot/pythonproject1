31bc9c5bb6198ede722d6c77c58f4dfb8e4dadfd
Ludelaplu <lucien.koot@gmail.com>
2019-06-17 14:32:28 +0200

    add val in dict2

--- startpart2.py
+++ startpart2.py
@@ -3,7 +3,8 @@ dict1 = {"Voornaam" :"gerard",  "Achternaam" : "jansen", "Leeftijd": 16}
 dict2 = {"waarde1": 1000, "waarde2": 2000}
 
 #adding values
+dict1 ["Hobby"]= "bowlen"
 dict2 ["waarde3"] = 3000
 
-print (dict1["Voornaam"])
+print (dict1["Voornaam"], dict1["Hobby"])
 print(dict2["waarde2"] ,dict2 [ "waarde3"])
