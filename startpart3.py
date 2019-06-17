6a8d6963d934af925b9d8a1781c9ea525b1ca25f
Ludelaplu <lucien.koot@gmail.com>
2019-06-17 14:47:09 +0200

    del val in dict3

---
+++ startpart3.py
@@ -0,0 +1,10 @@
+
+dict1 = {"Voornaam" :"gerard",  "Achternaam" : "jansen", "Leeftijd": 16}
+dict2 = {"waarde1": 1000, "waarde2": 2000}
+
+#removing values
+dict1 ["Hobby"]= "bowlen"
+dict2 ["waarde3"] = 3000
+del dict2 ["waarde1"]
+
+print ((dict1), (dict2))
