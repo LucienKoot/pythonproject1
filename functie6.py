6423ed0436df5f75e81ccd5a1dce815d1cdc8f95
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 11:41:01 +0200

    function for creating a song for a lot of people finish with more parties and completing manual

--- functie6.py
+++ functie6.py
@@ -5,7 +5,7 @@ def hvPP(persoon):
     print(" h - v  -  h  -  v")
     print("1 -  2  -  3  -  4")
     print(" h - v  ....")
-    print(persoon + "\nPAPIER !")
+    print(persoon + "\n(zing heel hard):  PAPIER  !!!")
     print(" ")
     return
 
@@ -13,4 +13,7 @@ hvPP( 'Meisjes :' )
 hvPP( 'Jongens :' )
 hvPP( 'Transgenders :' )
 hvPP('Bejaarden :')
+hvPP('De mannen :')
+hvPP("De vrouwen :")
+hvPP('en als laatsten...\nDe Vegetariërs :')
      
