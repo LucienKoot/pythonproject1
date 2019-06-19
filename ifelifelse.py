81e4267a393d30ea502b735a075c0ef67be64561
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 16:16:49 +0200

    if elif else

---
+++ ifelifelse.py
@@ -0,0 +1,19 @@
+var = 10
+waarde = 7
+if waarde == var:
+   print ("De variabele is", var, ". Dit voldoet dus WEL aan de gestelde waarde van", waarde,)
+ 
+elif waarde+1 == var:
+   print ("De variabele is", var, ". Dit voldoet dus WEL aan waarde", waarde,"+1")
+ 
+elif waarde+2 == var:
+   print ("De variabele is", var, ". Dit voldoet dus WEL aan waarde", waarde,"+2")
+ 
+elif waarde+3 == var:
+   print ("De variabele is", var, ". Dit voldoet dus WEL aan waarde", waarde,"+3")
+ 
+else:
+   print ("De variabele is", var, ". Dit voldoet dus NIET aan de gestelde waarde van", waarde, "of andere acceptabele waardes")
+
+
+
