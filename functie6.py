36985e7bbcef09306f4ba7e29e37c8bafad89510
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 11:17:11 +0200

    function for creating a song for a lot of people

---
+++ functie6.py
@@ -0,0 +1,16 @@
+
+def hvPP(persoon):
+    "liedje arrangeren voor een jongens en meisjes koor."
+    print("1 -  2  -  3  -  4")
+    print(" h - v  -  h  -  v")
+    print("1 -  2  -  3  -  4")
+    print(" h - v  ....")
+    print(persoon + "\nPAPIER !")
+    print(" ")
+    return
+
+hvPP( 'Meisjes :' )
+hvPP( 'Jongens :' )
+hvPP( 'Transgenders :' )
+hvPP('Bejaarden :')
+     
