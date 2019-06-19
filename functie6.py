d4f7f952475bf6dce2de551a99c381930e2f04c0
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 12:01:23 +0200

    REGULIERE EXPRESSIE EN ESCAPES

--- functie6.py
+++ functie6.py
@@ -1,11 +1,11 @@
 
-def hvPP(persoon):
+def hvPP(groep):
     "liedje arrangeren voor een jongens en meisjes koor."
     print("1 -  2  -  3  -  4")
     print(" h - v  -  h  -  v")
     print("1 -  2  -  3  -  4")
     print(" h - v  ....")
-    print(persoon + "\n(zing heel hard):  PAPIER  !!!")
+    print(groep + "\n(zing heel hard):  PAPIER  !!!")
     print(" ")
     return
 
