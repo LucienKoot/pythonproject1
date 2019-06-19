25bdd38b769afe4872b914dc8095c887e479fd0a
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 10:10:03 +0200

    functions4, adding \ren empty line, singing for 3 persons, functie loopt niet

---
+++ functie5.py
@@ -0,0 +1,10 @@
+
+
+def hvpp(persoon):
+    "liedje voor een koor maken"
+    print("één twee drie vier..\nHoedje van, Hoedje van,\néén twee drie vier..\nHoedje van....")
+    print("Nu roept "+ persoon + "heel hard: P A P I E R ! ! !")
+    print("applaus.....")
+    return
+
+    hvpp('Pietje')
