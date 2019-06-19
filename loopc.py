cde5676dff5827d22822c61b6cde96551afd5a3a
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 15:51:05 +0200

    for loop list3

--- loopc.py
+++ loopc.py
@@ -2,6 +2,9 @@ fruitmand = ["appel", "peer", "mango", "kersen", "zeemeermin",  "banaan"]
 for fruit in fruitmand:
     print("In de mand:  ", fruit [0])
 
+for x in range (4):
+    print (x)
+
 
 
 
