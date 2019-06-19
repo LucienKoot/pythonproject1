6c03e62294060fb32defd9c887e6d7bf35f7972c
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 06:28:59 +0200

    functions

---
+++ functie1.py
@@ -0,0 +1,12 @@
+def happyBirthday(persoon):
+    "Zing Happy Birthday voor alle opgegeven personen."
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, lieve " + persoon + ".")
+    print("Happy Birthday to you!")
+    return
+ 
+happyBirthday('Harrie')
+happyBirthday('Andre')
+happyBirthday('Tineke')
+
