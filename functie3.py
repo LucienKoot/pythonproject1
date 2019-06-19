0d3acb9c0db7cb35df8a21090d82d20977271508
Ludelaplu <lucien.koot@gmail.com>
2019-06-19 09:52:23 +0200

    functions4, adding \ren empty line, singing for 3 persons

--- functie3.py
+++ functie3.py
@@ -1,4 +1,13 @@
 
-
-def 1234(naam)
-
+def happyBirthday(persoon):
+    "Zing Happy Birthday voor alle opgegeven personen."
+    print("Happy Birthday to you! \nHappy Birthday to you")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, lieve " + persoon + ".")
+    print("Happy Birthday to you! \nLet's party!!!!!")
+    print(" ")
+    return
+ 
+happyBirthday('Annie')
+happyBirthday('Andre')
+happyBirthday('Tineke')
