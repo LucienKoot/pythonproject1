b6d01d05e68bfd24174accbfa387c9f95a2404db
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 10:01:36 +0200

    while

--- forloop.py
+++ forloop.py
@@ -1,14 +1,7 @@
+# whilel loop
 
-# NOG EEN FOR LOOPJE
-
-nummers = [2,4,6, 11]
-
-for num in nummers:
-   
-   if num%2 == 0:
-         print ('Deze lijst heeft 3 getallen door 2 deelbaar, even nummer.')
-         break
-   else:
-        print("11 is nergens deelbaar door...")
-             
+teller = 0
+while (teller < 4):
+   print("Het aantal biljetten is", teller)
+   teller = teller + 1
              
