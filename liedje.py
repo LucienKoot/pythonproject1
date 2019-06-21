fc9490c84ab4ad61a25a6ac829c193f6307e1346
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 08:23:45 +0200

    if elif else3

---
+++ liedje.py
@@ -0,0 +1,17 @@
+def hvPP(persoon):
+    "liedje arrangeren voor een jongens en meisjes koor."
+    print("1 -  2  -  3  -  4")
+    print(" h - v  -  h  -  v")
+    print("1 -  2  -  3  -  4")
+    print(" h - v  ....")
+    print(persoon + "\n(zing heel hard):  PAPIER  !!!")
+    print(" ")
+    return
+
+hvPP( 'Meisjes :' )
+hvPP( 'Jongens :' )
+hvPP( 'Transgenders :' )
+hvPP('Bejaarden :')
+hvPP('De mannen :')
+hvPP("De vrouwen :")
+hvPP('en als laatsten...\nDe Vegetariërs :')
