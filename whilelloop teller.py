f9d3e5430e2709fe895dfc596b240ec49cb341ff
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 12:49:32 +0200

    whileloop oef

---
+++ whilelloop teller.py
@@ -0,0 +1,8 @@
+# whilel loop, teller
+teller =6
+
+while (teller < 5):
+   print ("Het huidige getal is kleiner dan:", teller)
+   teller = teller + 1
+else:
+   print ("Het huidige getal is gelijk aan of groter dan:", teller)
