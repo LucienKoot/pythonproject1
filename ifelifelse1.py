0c4d61669900ad5246f9647facf3a81909c4d762
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 09:10:08 +0200

    break statement.

--- ifelifelse1.py
+++ ifelifelse1.py
@@ -1,17 +1,10 @@
 
 
 
-# de achtbaan
+# Break statement.
 
-
-kindLengte = 180
-eis = 150
-
-if kindLengte ==eis:
-   print ("Jij mag mee, want je bent: ",kindLengte ," cm, maar neem een begeleider mee")
-
-elif kindLengte <= eis:
-   print("Minimum lengte is", eis ,"cm, helaas je mag niet mee, want jij bent:  ", kindLengte, "centimeter. "  )
-
-else :
-   print ("Jij mag mee, want je bent: ",kindLengte - eis ," cm langer.")
+for letter in "Appeltaart":
+   if letter == "t":
+      break
+   print ("In het scherm: ", letter)
+   
