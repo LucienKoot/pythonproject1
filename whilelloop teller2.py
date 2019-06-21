87faa1639f0169388b934984d6473417a5e12b9f
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 13:03:14 +0200

    whileloop uitr

---
+++ whilelloop teller2.py
@@ -0,0 +1,11 @@
+# whilel loop, teller
+teller = 6
+while (teller < 5):
+   print("Kleiner dan:", teller)
+   teller = teller + 1
+
+if teller == 5 :
+   print("Gelijk aan:", teller)
+
+elif teller > 5:
+   print("Groter dan: 5")
