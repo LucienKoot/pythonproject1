1f03b25336c041f229442c61ce8dd1249fd7d5ce
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 09:43:00 +0200

    nog een forloop.

---
+++ forloop.py
@@ -0,0 +1,14 @@
+
+# NOG EEN FOR LOOPJE
+
+nummers = [2,4,6, 11]
+
+for num in nummers:
+   
+   if num%2 == 0:
+         print ('Deze lijst heeft 3 getallen door 2 deelbaar, even nummer.')
+         break
+   else:
+        print("11 is nergens deelbaar door...")
+             
+             
