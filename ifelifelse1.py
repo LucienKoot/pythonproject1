7beb7eb4c014e00ee0354f89def332c854336652
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 08:38:28 +0200

    if elif else4

--- ifelifelse1.py
+++ ifelifelse1.py
@@ -4,8 +4,14 @@
 # de achtbaan
 
 
-kindLengte = 120
+kindLengte = 180
 eis = 150
 
-if kindLengte <= eis:
+if kindLengte ==eis:
+   print ("Jij mag mee, want je bent: ",kindLengte ," cm, maar neem een begeleider mee")
+
+elif kindLengte <= eis:
    print("Minimum lengte is", eis ,"cm, helaas je mag niet mee, want jij bent:  ", kindLengte, "centimeter. "  )
+
+else :
+   print ("Jij mag mee, want je bent: ",kindLengte ," cm, dat is lang genoeg")
