80c40cf0fa8f015fdf7b064a5f69dcf73ed3c509
Ludelaplu <lucien.koot@gmail.com>
2019-06-24 11:34:56 +0200

    format string3

---
+++ stringformat2.py
@@ -0,0 +1,14 @@
+# formatting strings
+
+
+String1 = 'Lucien'
+String2 = 'Koot'
+String3 = 'Aardig'
+String4 = 'Gek'
+String5 = 'Schouderklopjes'
+String6 = 'Klappen'
+Nummer1 = 100
+Nummer2 = 200
+Nummer3 = 300
+
+print ("%s  %s is %s en krijgt %d %s" % (String1, String2, String3, Nummer3, String6))
