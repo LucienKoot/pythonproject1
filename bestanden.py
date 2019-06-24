5877372596b1407851f2b0fa8ff41a4fea334e18
Ludelaplu <lucien.koot@gmail.com>
2019-06-24 12:49:54 +0200

    werken met bestanden

---
+++ bestanden.py
@@ -0,0 +1,6 @@
+bestand = open("test.txt", "wb")
+bestand.write(bytes("Dit is de eerste tekst in het bestand.\n", "UTF-8"))
+bestand.close()
+
+print (bestand.mode)
+print (bestand.name)
---
+++ bestandtxttoevoeg.py
@@ -0,0 +1,5 @@
+bestand = ab+("test.txt", "ab+")
+
+bestand.write(bytes("Dit is het tweede stukje tekst in dit bestandje.... lalala.\n "UTF-8"))
+bestand.close()
+                    
