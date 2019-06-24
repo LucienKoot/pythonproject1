8fdeb2cf5d5416c8d0a6b9ba1dc70ed1d59ef050
Ludelaplu <lucien.koot@gmail.com>
2019-06-24 11:59:04 +0200

    get command

---
+++ getcommand1.py
@@ -0,0 +1,4 @@
+Woordenboek = { 'lekker' :  'aantrekkelijk, 'vies' :  'smerig'}
+                print ("Value : %s" % Woordenboek.get ('lekker', "Lekker komt niet voor in het woordenboek"))
+                print ("Value : %s" % Woordenboek.get ('gadver', "gadver komt niet voor in het woordenboek"))
+                
---
+++ getcommand1a.py
@@ -0,0 +1,4 @@
+Synoniemwoordenboek = {'Lekker': 'Aantrekkelijk', 'Vies': 'Smerig'}
+ 
+print ("Value : %s" %  Synoniemwoordenboek.get('Lekker', "Lekker komt niet voor in het woordenboek"))
+print ("Value : %s" %  Synoniemwoordenboek.get('Jummie', "Jummie komt niet voor in het woordenboek"))
