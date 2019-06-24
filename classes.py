d784f7b7fcd0a148caea5401385b25cbbcfc5491
Ludelaplu <lucien.koot@gmail.com>
2019-06-24 16:54:51 +0200

    classesvervolg

--- classes.py
+++ classes.py
@@ -2,3 +2,9 @@ class Hout:
     "Deze class bevat alle eigenschappen van onze houtsoorten"
     aantHout = 0
     
+    def _init_(self, naam, gewicht, lengte, breedte):
+        self.naam = naam
+        self.gewicht = gewicht
+        self.lengte = lengte
+        self.breedte = breedte
+        Hout.aantHout += 1
