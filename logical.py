e646f9f22b27d6acd7315f2da04893041319f6d5
Ludelaplu <lucien.koot@gmail.com>
2019-06-24 09:17:22 +0200

    logical operators3

--- logical.py
+++ logical.py
@@ -1,12 +1,15 @@
 # fietsenhandel
 
-leeftijd = 7
+leeftijd = 24
 
 if (leeftijd >= 1) and (leeftijd <=4):
     print ("Achter op bij pappa of mamma !")
 
 elif (leeftijd >=5) and (leeftijd <=11):
     print ("Jij krijgt een kinderfiets !")
+
+elif ((leeftijd >=12) and (leeftijd <=20)) and ((leeftijd >=22) and (leeftijd<=59)):
+    print ("Wij reserveren een volwassenen fiets.")
 
 
 
