ffeab8cb6d86369a57635029a4fe4c88f818e463
Ludelaplu <lucien.koot@gmail.com>
2019-06-24 08:57:34 +0200

    logical operators1

---
+++ logical.py
@@ -0,0 +1,9 @@
+# fietsenhandel
+
+leeftijd = 16
+
+if ((leeftijd >= 1) and (leeftijd <=4)):
+    print ("Achter op bij pappa of mamma")
+
+else:
+    print ("Dat wordt reserveren voor een bierfiets")
